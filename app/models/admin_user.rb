649faaba18882f1bfea77e908164af955cdf14f5
chrisjohnson89 <afckazoo@gmail.com>
2014-06-04 23:19:54 -0700

    Final Commit

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,8 +1,8 @@
 class AdminUser < ActiveRecord::Base
 
-	#To configure a diffrent table name:
-	#self.table_name = "admin_users"
+  # To configure a different table name:
+  # self.table_name = "admin_users"
+
+  has_and_belongs_to_many :pages
 
 end
-
-
